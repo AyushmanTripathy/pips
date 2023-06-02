ebceb90980d03707c3b35eb3d36f66c3debb41e2
AyushmanTripathy <ayushmantripathy2004@gmail.com>
2023-06-02 16:17:43 +0530

    removed junk

--- index.js
+++
@@ -1,7 +0,0 @@
-function fibb(x) {
-  if (x == 1) return 1;
-  else if (x == 0) return 0;
-  return fibb(x - 1) + fibb(x - 2);
-}
-
-console.log(fibb(30));
--- index.py
+++
@@ -1,5 +0,0 @@
-def fibb(target, x, a, b):
-    if (x == target): return a
-    return fibb(target, x + 1, a + b, a)
-
-print(fibb(30, 1, 1, 0))
